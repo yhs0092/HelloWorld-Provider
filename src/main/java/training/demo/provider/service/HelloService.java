823c58fc67a9b1d6b77210c4e060b796b5eef39e
yaohaishi <yaohaishi@huawei.com>
2018-12-21 17:43:00 +0800

    add dynamic config

--- src/main/java/training/demo/provider/service/HelloService.java
+++ src/main/java/training/demo/provider/service/HelloService.java
@@ -5,11 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.netflix.config.DynamicPropertyFactory;
+import com.netflix.config.DynamicStringProperty;
+
 @RestSchema(schemaId = "hello") // 该注解声明这是一个REST接口类，CSEJavaSDK会扫描到这个类，根据它的代码生成接口契约
 @RequestMapping("/hello")  // @RequestMapping是Spring的注解，这里在使用Spring MVC风格开发REST接口
 public class HelloService {
+  private DynamicStringProperty sayHelloPrefix = DynamicPropertyFactory.getInstance()
+      .getStringProperty("hello.sayHello", "");
+
   @RequestMapping(value = "/{name}", method = RequestMethod.GET)
   public String sayHello(@PathVariable(value = "name") String name) {
-    return "Hello, " + name;
+    return sayHelloPrefix.get() + name;
   }
 }
