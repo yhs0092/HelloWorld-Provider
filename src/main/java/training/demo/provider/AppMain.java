05c158167b6d530d595fd55ec4d2abb49f128ea8
yaohaishi <yaohaishi@huawei.com>
2018-12-21 09:19:05 +0800

    add comment

--- src/main/java/training/demo/provider/AppMain.java
+++ src/main/java/training/demo/provider/AppMain.java
@@ -5,7 +5,7 @@ import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
 
 public class AppMain {
   public static void main(String[] args) throws Exception {
-    Log4jUtils.init();
-    BeanUtils.init();
+    Log4jUtils.init(); // 初始化默认的日志组件
+    BeanUtils.init();  // 加载Spring bean定义文件，正式开始启动流程
   }
 }
--- src/main/java/training/demo/provider/service/HelloService.java
+++ src/main/java/training/demo/provider/service/HelloService.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestSchema(schemaId = "hello")
-@RequestMapping("/hello")
+@RestSchema(schemaId = "hello") // 该注解声明这是一个REST接口类，CSEJavaSDK会扫描到这个类，根据它的代码生成接口契约
+@RequestMapping("/hello")  // @RequestMapping是Spring的注解，这里在使用Spring MVC风格开发REST接口
 public class HelloService {
   @RequestMapping(value = "/{name}", method = RequestMethod.GET)
   public String sayHello(@PathVariable(value = "name") String name) {
